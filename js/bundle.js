a29132ff49160e0f29f317078992a49563b570b7
Mandeep <panesarmonika89@gmail.com>
2018-06-05 13:17:34 -0400

    call openweatherapi with variable countryName, call getData() with enter pressed, added country_name field with text-transform: capitalize

--- js/bundle.js
+++ js/bundle.js
@@ -1,20 +1,34 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+var countryName;
 var request = require('request');
-
 function getData() {
-    request('http://api.openweathermap.org/data/2.5/weather?q=London&appid=8d7071faebf065763f900f514ca03c76', function (error, response, body) {
+    countryName = document.querySelector("#country_name").value;
+    request('http://api.openweathermap.org/data/2.5/weather?q='+encodeURI(countryName)+'&units=metric&appid=8d7071faebf065763f900f514ca03c76', function (error, response, body) {
     //   console.log('error:', error); // Print the error if one occurred
     //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     //   console.log('body:', body); // Print the HTML for the Google homepage.
     if (error) {
         console.log("something went wrong");
+        document.getElementById("temp").innerHTML = "Connection error";
         console.log(error);
     } else {
         var parsedData = JSON.parse(body);
         console.log(parsedData);
+        if (parsedData['cod'] == 200) {
+            document.getElementById("temp").innerHTML = parsedData['main']['temp'] + " &deg;C";
+        } else {
+            document.getElementById("temp").innerHTML = "City not found";
+        }
+        //window.alert(parsedData);
     }
     });
 }
+document.querySelector("#country_name").onkeyup = function() {
+    if (event.keyCode == 13) {
+        console.log("Enter pressed!");
+        getData();
+    }
+};
 document.getElementById("myBtn").onclick = getData;
 
 },{"request":273}],2:[function(require,module,exports){
