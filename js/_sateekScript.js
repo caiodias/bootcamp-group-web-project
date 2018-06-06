f53364960dcf940f37c66cab535030ea803e93d5
Sateek <sateeksuper@gmail.com>
2018-06-05 20:29:30 -0400

    Implemented the search box, implemented dynamic dom creation to show image data in grid format

--- js/_sateekScript.js
+++ js/_sateekScript.js
@@ -1,20 +1,24 @@
 var request = require('request');
 var parsedData = {};
 function getData() {
-    $('#resultsDiv').empty();
-    request('https://pixabay.com/api/?key=9176852-528521b115002c31dd0c1b967&q=orange&image_type=photo&total=20&page=1&per_page=10', function (error, response, body) {
+    var query = $('#searchBox').val();
+    $('#resultsContainer').empty();
+    request('https://pixabay.com/api/?key=9176852-528521b115002c31dd0c1b967&q=' + encodeURI(query) + '&image_type=photo&total=20&page=1&per_page=10', function (error, response, body) {
         // console.log('error:', error); // Print the error if one occurred
         // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         // console.log('body:', body); // Print the HTML for the Google homepage.
         if(error){
             console.log('Connection Failed');
+            $('#resultsContainer').append("<div class='row'><p>Connection error!</p></div>");
         } else {
             if (response.statusCode == 200) {
                 parsedData = JSON.parse(body);
                 console.log("Number of Images - " + parsedData['hits'].length);
+                console.log("Body", parsedData);
                 loadResults();
             } else {
                 console.log("Nothing found!");
+                $('#resultsContainer').append("<p>No images found</p>");
             }
         }
     });
@@ -24,11 +28,30 @@ function getData() {
 function loadResults() {
     console.log("Results loaded with data size - " + parsedData['hits'].length);
     for(let i = 0; i < parsedData['hits'].length; i++) {
-        $resultItem = "<div class='col-sm-3'>"
-                    +   "<p>asdfa</p>"
-                    + "</div>";
+        $resultItem = "<div class='shadow'>"
+                    +   "<div class='imgDiv'>"
+                    +       "<img class='image' src='"+ parsedData['hits'][i]['webformatURL'] +"'>"
+                    +   "</div></div>";
         console.log($resultItem);
-        $('#resultsDiv').append($resultItem);
+        $('#resultsContainer').append($resultItem);
+        
+        var image = $(".image");
+        var container = $(".imgDiv");
+        function getAspectRatio(element) {
+            var width = element.width();
+            var height = element.height();
+            return width / height;
+        }       
+        function centerImage() {
+            if (getAspectRatio(image) > getAspectRatio(container)) {
+                image.height("100%");
+                image.width("auto");
+            } else {
+                image.height("auto");
+                image.width("100%");
+            }
+        }
+        centerImage();
     }
 }
 
