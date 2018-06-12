9b25c821fde4ac64f44215bf794b2b515e07ae03
Sateek <sateeksuper@gmail.com>
2018-06-12 11:41:10 -0400

    changed 10 image count to 30, added check for no image found

--- js/_sateekScript.js
+++ js/_sateekScript.js
@@ -3,7 +3,7 @@ var parsedData = {};
 function getData() {
     var query = $('#searchBox').val();
     $('#resultsContainer').empty();
-    request('https://pixabay.com/api/?key=9176852-528521b115002c31dd0c1b967&q=' + encodeURI(query) + '&image_type=photo&total=20&page=1&per_page=10', function (error, response, body) {
+    request('https://pixabay.com/api/?key=9176852-528521b115002c31dd0c1b967&q=' + encodeURI(query) + '&image_type=photo&total=20&page=1&per_page=30', function (error, response, body) {
         // console.log('error:', error); // Print the error if one occurred
         // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         // console.log('body:', body); // Print the HTML for the Google homepage.
@@ -13,12 +13,16 @@ function getData() {
         } else {
             if (response.statusCode == 200) {
                 parsedData = JSON.parse(body);
-                console.log("Number of Images - " + parsedData['hits'].length);
-                console.log("Body", parsedData);
-                loadResults();
+                if(parsedData['hits'] == 0) {
+                    $('#resultsContainer').append("<p>No Images found!</p>");
+                } else {
+                    console.log("Number of Images - " + parsedData['hits'].length);
+                    console.log("Body", parsedData);
+                    loadResults();
+                }
             } else {
                 console.log("Nothing found!");
-                $('#resultsContainer').append("<p>No images found</p>");
+                $('#resultsContainer').append("<p>Error!</p>");
             }
         }
     });
@@ -30,7 +34,7 @@ function loadResults() {
     for(let i = 0; i < parsedData['hits'].length; i++) {
         $resultItem = "<div class='shadow'>"
                     +   "<div class='imgDiv'>"
-                    +       "<img class='image' src='"+ parsedData['hits'][i]['webformatURL'] +"'>"
+                    +       "<a href='"+ parsedData['hits'][i]['largeImageURL'] + "' target='_blank'><img class='image' src='"+ parsedData['hits'][i]['webformatURL'] +"'></a>"
                     +   "</div></div>";
         console.log($resultItem);
         $('#resultsContainer').append($resultItem);
